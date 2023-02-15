58d563ac37e58bdc450d83464910931d6bf9988b
pharase <diman230598@mail.ru>
2023-02-15 20:06:32 +0300

    Corrected dataclass Engine

--- homework_02/engine.py
+++ homework_02/engine.py
@@ -4,6 +4,5 @@ create dataclass `Engine`
 from dataclasses import dataclass
 @dataclass
 class Engine:
-    def __init__(self, volume = 1.6, pistons = 4):
-        self.volume = volume
-        self.pistons = pistons
+        volume : float
+        pistons : int
